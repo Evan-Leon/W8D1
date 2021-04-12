323a9b4544afcb002f269615fe3e06829235c834
Evan-Leon <evan.leon.j@gmail.com>
2021-04-12 13:27:51 -0400

    Added sessions and user controllers

---
+++ reddit_app/app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+    before_action :ensure_logged_in, only: [:destroy]
+
+    def create 
+        @user = User.find_by_credentials(params[:user][:username], params[:user][:username])
+
+        if @user 
+            login!(@user)
+            redirect_to user_url(@user)
+        else  
+            flash.now[:errors] = ['Incorrect username or password']
+            render :new 
+        end
+    end
+
+    def destroy 
+        logout!
+        render :new 
+    end
+
+end
---
+++ reddit_app/app/controllers/users_controller.rb
@@ -0,0 +1,56 @@
+class UsersController < ApplicationController
+
+    before_action :ensure_logged_in, only: [:destroy]
+    def index 
+        @users = User.all 
+
+        render :index
+    end
+
+    def show 
+        @user = User.find(params[:id])
+
+        render :show 
+    end
+
+    def new 
+        @user = User.new 
+
+        render :new 
+    end
+
+    def create 
+        @user = User.new(user_params)
+
+        if @user.save 
+            login!(@user)
+
+            redirect_to users_url 
+        else  
+            flash.now[:errors] = @user.errors.full_messages
+            render :new 
+        end
+    end
+
+    def edit 
+        @user = User.find(params[:id])
+
+        render :edit
+    end
+
+    def update 
+        @user = User.find(params[:id])
+
+        if @user.update_attributes(user_params)
+            flash.now[:success] = ["Successfully edited #{@user}!"]
+            render :index 
+        else  
+            flash.now[:errors] = ['Inccorect username or password']
+            render :edit 
+        end
+    end
+
+    def user_params 
+        params.require(:user).permit(:username, :password)
+    end
+end
